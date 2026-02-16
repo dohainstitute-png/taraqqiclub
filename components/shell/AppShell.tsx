1b6aeaed0b11b3417ed91ec16f538201ef918452
dohainstitute <dohainstitute@gmail.com>
2026-02-16 20:44:09 +0000

    After AppShell update

--- components/shell/AppShell.tsx
+++ components/shell/AppShell.tsx
@@ -1,32 +1,15 @@
 "use client";
 
 import type { ReactNode } from "react";
-import { CreateProvider } from "./create-context";
-import TopBar from "./TopBar";
+import TopAppBar from "./TopAppBar";
 import BottomNav from "./BottomNav";
-import CreateEntryButton from "./CreateEntryButton";
-import CreateSheet from "./CreateSheet";
 
-export default function AppShell({
-  children,
-  locale,
-}: {
-  children: ReactNode;
-  locale: string;
-}) {
+export default function AppShell({ children }: { children: ReactNode }) {
   return (
-    <CreateProvider>
-      <div className="topbar">
-        <TopBar locale={locale} />
-      </div>
-
-      <div className="shell">
-        <div className="container">{children}</div>
-      </div>
-
-      <CreateEntryButton locale={locale} />
-      <BottomNav locale={locale} />
-      <CreateSheet locale={locale} />
-    </CreateProvider>
+    <div className="flex flex-col h-screen">
+      <TopAppBar />
+      <main className="flex-1 overflow-y-auto">{children}</main>
+      <BottomNav />
+    </div>
   );
 }
